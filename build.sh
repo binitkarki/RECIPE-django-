7405d2cbb2dd211c8bd15c27d25505756af8457c
Binit Karki <binitkarki@gmail.com>
2026-01-08 22:21:54 +0545

    .

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
-set -o errexit
+#!/bin/bash
+set -o errexit  # Exit if any command fails
 
+# Install dependencies
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
 
+# Apply migrations
 python manage.py migrate
 
-if[[$CREATE_SUPERUSER]];
-then
+# Create superuser if environment variable is set
+if [[ "$CREATE_SUPERUSER" == "True" ]]; then
     python manage.py createsuperuser --no-input
-fi    
+fi
